89d29e0ebd3e83bd205d4798679daf88bafb2639
JDMTL5150 <jacobd2887@gmail.com>
2025-10-01 18:09:08 -0400

    Version 1.0.1 - added clipboard copy abilities for code snippets and added back to top button that hides unless scrolled far enough. - Jacob Dawes

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-const backToTop = document.getElementById("back-to-top");
 
-window.addEventListener("scroll", () => {
+
+
+// code for copying code snippets to clipboard
+const codeElements = document.querySelectorAll('.code-copy');
+
+codeElements.forEach(element => {
+    element.addEventListener('click', () => {
+        const code = element.innerText;
+        navigator.clipboard.writeText(code)
+            .then(() => {
+                const prev = element.innerText;
+                element.innerText = 'Copied!';
+                const prevColor = element.style.color;
+                element.style.color = '#32CD32';
+                setTimeout(() => {element.innerText = prev; element.style.color = prevColor;}, 1000);
+            })
+            .catch(err => {
+                console.error('Failed to copy text: ', err);
+            });
+    });
+});
+
+// end of code for copying code snippets to clipboard
+
+// code for back to top button
+const backToTopButton = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
     if (window.scrollY > 300) {
-        backToTop.classList.add("show"); // show when scrolled down
+        backToTopButton.classList.add('show');
     } else {
-        backToTop.classList.remove("show"); // hide at the top
+        backToTopButton.classList.remove('show');
+        
     }
-})
+});
+// end of code for back to top button
